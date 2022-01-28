3a840a48a9b94844a865f4ac204ffe6b2fbbe3eb
Lyubov Danilova <sniper@Lyubovs-Air.fritz.box>
2022-01-29 01:39:17 +0300

    add Send {surname: "Colombo"}

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,17 +36,20 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+          .send({
+            "surname": "Colombo"
+          })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
       assert.fail();
-
       done();
     });
   });
